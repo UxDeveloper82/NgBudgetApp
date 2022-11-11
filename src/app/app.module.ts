2f5ad1b16c86577982aab4643bfccec5da759ab3
Martin Fernandez <Electronicsmf@aol.com>
2022-11-11 07:00:22 -0500

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { EditItemModalComponent } from './edit-item-modal/edit-item-modal.component';
+import { BudgetItemListComponent } from './budget-item-list/budget-item-list.component';
+import { AddItemFormComponent } from './add-item-form/add-item-form.component';
+import {MatDialogModule} from '@angular/material/dialog';
+import { BudgetItemCardComponent } from './budget-item-list/budget-item-card/budget-item-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    EditItemModalComponent,
+    BudgetItemListComponent,
+    AddItemFormComponent,
+    BudgetItemCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatDialogModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
